e50135c2e88c0c33bec45db752dd0e16617e9a30
iven_k <koethke@th-brandenburg.de>
2019-08-05 04:38:33 +0200

    Store port in module level variable/const

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 import uvicorn
 
+PORT=8080
+
+
 app = Starlette(debug=True)
 
 @app.route('/')
@@ -9,4 +12,4 @@ async def homepage(request):
     return JSONResponse({'hello':'world'})
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8080)
+    uvicorn.run(app, host='0.0.0.0', port=PORT)
